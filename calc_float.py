378230b4f1f284af2725712d890b2efac9121fb6
AlexKri72 <krial11@rambler.ru>
2022-05-19 23:26:58 +0500

    calc_float

---
+++ calc_float.py
@@ -0,0 +1,10 @@
+
+def calc(a:float,b:float,operation:str):
+    if operation=='+':
+        return a+b
+    if operation=='-':
+        return a-b
+    if operation=='*':
+        return a*b
+    if operation=='/':
+        return a/b
