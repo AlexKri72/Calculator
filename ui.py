4acad00fe1952d3b5fc00d750e7cb662f5531cc0
AlexKri72 <krial11@rambler.ru>
2022-05-19 23:36:05 +0500

    ui

---
+++ ui.py
@@ -0,0 +1,10 @@
+from re import A
+
+
+global a
+a=input('Введите первое число: ')
+global b
+b=input('Введите второе число: ')
+
+global operation
+operation=input("Введите операцию ('+'/'-'/'*'/'/'): ")
