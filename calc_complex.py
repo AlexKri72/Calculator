ae03c2a0afd06518b633a2600d877f116d6b170e
AlexKri72 <krial11@rambler.ru>
2022-05-19 23:26:30 +0500

    calc_complex

---
+++ calc_complex.py
@@ -0,0 +1,10 @@
+
+def calc(a:complex,b:complex,operation:str):
+    if operation=='+':
+        return a+b
+    if operation=='-':
+        return a-b
+    if operation=='*':
+        return a*b
+    if operation=='/':
+        return a/b
